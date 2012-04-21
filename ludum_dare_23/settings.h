ed783e367b360b3210a8f36484527775aa6d172d
beersbr <beersbr@gmail.com>
2012-04-20 23:32:57 -0600

    Updated the settings

--- ludum_dare_23/settings.h
+++ ludum_dare_23/settings.h
@@ -1,13 +1,19 @@
 #pragma once
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 const int MAX_X_SIZE = 100;
 const int MAX_Y_SIZE = 100;
 const int X_TILE_SIZE = 40;
 const int Y_TILE_SIZE = 40;
+
+const int REZ_X = 1200;
+const int REZ_Y = 800;
+
+const int FRAME_LIMIT = 60;
+
+const std::string WINDOW_TITLE = "Ludum Dare <TO BE NAMED>";
 
 enum GAME_STATE{STOPPED = 0, RUNNING, PAUSED, LOADING};
