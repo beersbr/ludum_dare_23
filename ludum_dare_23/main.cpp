3769079d607eeb2ae2e5a29428e75cb52565fca7
beersbr <beersbr@gmail.com>
2012-04-20 21:36:37 -0600

    new code

---
+++ ludum_dare_23/main.cpp
@@ -0,0 +1,10 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+
+int main()
+{
+	sf::RenderWindow app(sf::VideoMode(800, 600, 32), "test");
+
+	return EXIT_SUCCESS;
+}
